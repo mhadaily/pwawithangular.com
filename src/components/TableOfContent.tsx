454669faffcde0edc363709e036119d19c080159
Majid Hajian <mhadaily@gmail.com>
2019-05-23 00:45:12 +0200

    simplify prevewImages graphQL query

---
+++ src/components/TableOfContent.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const TableOfContent = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        fragment CommonOptimization on File {
+          childImageSharp {
+            fluid(maxWidth: 900, quality: 80) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        query {
+          prev_1: file(relativePath: { eq: "assets/previews/1.png" }) {
+            ...CommonOptimization
+          }
+          prev_2: file(relativePath: { eq: "assets/previews/2.png" }) {
+            ...CommonOptimization
+          }
+          prev_3: file(relativePath: { eq: "assets/previews/3.png" }) {
+            ...CommonOptimization
+          }
+          prev_4: file(relativePath: { eq: "assets/previews/4.png" }) {
+            ...CommonOptimization
+          }
+          prev_5: file(relativePath: { eq: "assets/previews/5.png" }) {
+            ...CommonOptimization
+          }
+          prev_6: file(relativePath: { eq: "assets/previews/6.png" }) {
+            ...CommonOptimization
+          }
+        }
+      `}
+      render={(data: any) => (
+        <section className="section features-carousel b-b">
+          <div className="container">
+            <div className="section-heading mb-6 text-center">
+              <div className="bold small text-uppercase pb-4">Overview of</div>
+              <h2>Table of Contents</h2>
+            </div>
+            <div className="row">
+              {Object.keys(data).map((key, i) => {
+                return (
+                  <div className="col-md-4 py-4 rounded shadow-hover text-center" key={i}>
+                    <div className="card border-0 shadow">
+                      <div className="card-body">
+                        <Img fluid={data[key].childImageSharp.fluid} className="img-responsive" alt="Book table of contents" />
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      )}
+    />
+  );
+};
+
+export default TableOfContent;
